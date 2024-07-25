4851d0b2bc76b1c86c4dd57f45d18a843542fba5
samar-essam <samaresam243@gmail.com>
2024-07-25 16:10:06 +0300

    second commit

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 
 export const dbConnection = ()=>{
-    mongoose.connect('mongodb://0.0.0.0:27017/sara7a')
+    mongoose.connect('mongodb+srv://samarEssam:gQVCo8HX6ouygB0o@cluster0.6hiiuac.mongodb.net/sara7a-app')
     .then( ()=> console.log("db connected"))
     .catch( (err)=> console.log( "database err",err))
 }
